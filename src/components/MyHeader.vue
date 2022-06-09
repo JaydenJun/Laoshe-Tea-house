0d4a38e1804e82527a7ac31098f21f96348c14b7
tao <tao@163.cn>
2022-06-09 19:05:08 +0800

    导航

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -4,14 +4,14 @@
       <el-row :gutter="10">
         <el-col :xs="24" :sm="5" :md="4"
           ><div class="grid-content bg-purple">
-            <div class="imgl">
+            <div>
               <img src="../assets/log2.png" alt="" />
             </div></div
         ></el-col>
         <el-col :xs="24" :sm="14" :md="16"
           ><div class="grid-content bg-purple">
             <div>
-              <ul class="cai">
+              <ul>
                 <li><router-link to="/">首页</router-link></li>
                 <li><router-link to="/">茶馆特色</router-link></li>
                 <li><router-link to="/teah">茶友圈</router-link></li>
@@ -26,11 +26,11 @@
           ><div class="grid-content bg-purple-light">
             <div>
               <ul>
-                <li>
+                <li style="width:32px">
                   <router-link to="/login">登录</router-link>
                 </li>
 
-                <li>
+                <li style="width:32px">
                   <router-link to="/register">注册</router-link>
                 </li>
               </ul>
@@ -47,30 +47,25 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-
 .container {
   max-width: 85vw;
   margin: 0 auto;
   margin-bottom: 30px;
   div:first-child {
-    text-align: center;
-    display: flex;
-    .imgl {  
-      img {
-        width: 100%;
-      }
+    text-align: right;
+
+    img {
+      margin-top: 7px;
+      width: 100%;
     }
   }
-  div {
-    display: flex;
-    justify-content: flex-end;
-  }
- 
   ul {
     list-style: none;
-    display: flex;
+    padding: 0;
+    display: inline-flex;
     li {
       margin: 0 1vw;
+      width: 64px;
       a {
         text-decoration: none;
         color: #333;
